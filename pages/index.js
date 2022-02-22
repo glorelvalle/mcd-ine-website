bd1719ca1055b3bab844dd460a72b897b7220a7f
glorelvalle <glorelvalle@gmail.com>
2022-02-22 17:54:45 +0100

    cambiado nombre

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>El origen del Sabaila-Doministiku</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
